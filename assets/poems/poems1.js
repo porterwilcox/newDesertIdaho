76214a740689cf63e9222ee86066eff756126892
Brett Wilcox <brettwilcox@truckstop.com>
2020-04-13 21:00:30 -0600

    Adding more poems.

--- assets/poems/poems1.js
+++ assets/poems/poems1.js
@@ -37,12 +37,239 @@ export default class Poems1 {
     He committed to stand with Julian, <br />
     As truth will break their spines.`
 
+    missteps = `
+    From missteps of good intentions <br />
+    The wanderer became unwelcome <br />
+    In his trek through gray-brown desert. <br />
+    Sagebrush began to whistle insults as he walked, <br />
+    The wind was unrelenting, gritty, oppressive, <br />
+    And a cold spring rainstorm pelted his face, <br />
+    Wearing him out, <br />
+    Weighing him down, <br />
+    Chafing his every step. <br />
+    Eventually he stumbled on a downslope, and fell, <br />
+    Landing hard upon a crooked rock, <br />
+    Injured and unsure. <br />
+    After a moment of uncertainty <br />
+    He stood, <br />
+    Tall and fierce, <br />
+    Knowing it would take infinitely more <br />
+    To break his resolute, western spirit.
+    `
+
+    falseHorizon = `
+    The sun sank, expectedly, <br />
+    Into the false horizon of our time. <br />
+    So much human folly, misery <br />
+    For no better reason <br />
+    Than centralized control of money, <br />
+    A tragic decision, <br />
+    And the invisible enemy, <br />
+    A virus, <br />
+    The tiniest of things <br />
+    Setting free the avalanche.  
+    `
+
+    hereBefore = `
+    In a forested backyard in desert Idaho, <br />
+    A late October chill confirms the autumn colors, <br />
+    Leaves, lying in a portrait on the dulling grass, <br />
+    Dogs chasing squirrels in viscous sprints, and <br />
+    Soft light still shallow in its orbit. <br />
+    Today will be unlike the others, <br />
+    Laughter, like just before the news, when <br />
+    Looking out the window in a forested backyard <br />
+    In a desert town in Idaho, on a <br />
+    Late October morning with a chill, <br />
+    Autumn colors — leaves, lying in a portrait — and<br />
+    Light shadows flickering from the apple tree branches. <br />
+    Maybe I have been here before.  
+    `
+
+    toCare = `
+    As I turn eyes westward<br />
+    A deeply penetrating orange light,<br />
+    Tangled in the hilltop trees,<br />
+    Brings a smile to Heraclitus and me.<br />
+    There’s fire-light running wild through the universe,<br />
+    Setting sun, late April Idaho brisk wind,<br />
+    Hiking trails; time.<br />
+    Hemingway never forgot his first love, the<br />
+    Memory of her more beautiful, abstract,<br />
+    Each passing day.<br />
+    Maybe our purpose is simple—<br />
+    To care, contribute, and<br />
+    Witness the splendor.  
+    `
+
+    chaosOfRipples = `
+    Ice appeared on the river’s banks today, and<br />
+    On the tops of rocks in the current,<br />
+    Changing the course of the world, from<br />
+    That which was, to<br />
+    That which now is, the<br />
+    Chaos of ripples shimmering then disappearing<br />
+    At the whim of cold passing clouds<br />
+    In the shallow morning sun.<br />
+    The river taught him years ago that<br />
+    Destiny is shaped by innumerable complexities,<br />
+    Owing nothing to precedence or righteousness, and<br />
+    Takes all routes to the same end, simultaneously.<br />
+    If you stop thinking for a moment, and<br />
+    Truly observe and listen,<br />
+    Nature will teach you what it means to be human,<br />
+    Allowing you to levitate just above the ground,<br />
+    For a while. 
+    `
+
+    crippledInSpirit = `
+    It was two hours before sky-dark<br />
+    When the badger was defeated,<br />
+    Crippled in spirit,<br />
+    Awkwardly bruised from the imperial battle,<br />
+    Humbled by the more capable creature<br />
+    Hell-bent on shaping the mountain<br />
+    In the image of its rage.<br />
+    A bird, soaring kingly in the ocean-gray sky,<br />
+    Observed the badger’s thrashing<br />
+    Through the autumn evening rain haze,<br />
+    Predicting the astringent badger would become<br />
+    Disloyal in character, acidic, unwell.<br />
+    In time, sadly, the badger proved the bird true,<br />
+    Plotting with a doltish, clumsy raven<br />
+    The humiliated badger prepared an attack<br />
+    To malign the bird’s account of the beating.<br />
+    Unbeknownst to the badger the spying eyes<br />
+    Of an eagle from afar also bore witness,<br />
+    Saving the bird from unholy disparage<br />
+    Claimed by the deficient duo.<br />
+    Sullen, the badger went back to its hole in the ground,<br />
+    The dimwitted raven knew not what to think.  
+    `
+
+    primordialRoutine = `
+    The fire light of desert sunrise,<br />
+    Lambent on the proud east walls of Red Rock Canyon,<br />
+    Repeats its primordial routine, dancing<br />
+    Over Spanish tiled roofs, palm trees, cactus.<br />
+    Oh, day— you are a peculiar stage—<br />
+    And I the lucky one<br />
+    To witness on a simple morning<br />
+    A quiet sunrise, and<br />
+    The ancient sky now peopled with airplanes<br />
+    Jetting triangle dreams that fade colors to blue.<br />
+    For a period of time<br />
+    When the sun is narrowly above the horizon<br />
+    The city between us disappears<br />
+    From the overpowering brightness of our star, yet<br />
+    Against all probability<br />
+    I am here.<br />
+    Today, anew.<br />
+    In this grand opera of sunlight<br />
+    And theater.  
+    `
+
+    aBeautifulLife = `
+    Maria Flores knew only a beautiful life,<br />
+    The flowered walls of her father’s Mexico estate<br />
+    Sheltered her from the pallid faces and<br />
+    Barrio clutter scattered about the village.<br />
+    Maria knew the sweet taste of fresh cornbread<br />
+    Peppered with green chiles grown in her garden;<br />
+    She knew her papa’s laugh, his smile, his kind voice.<br />
+    Maria knew many nice things, and such is a<br />
+    Lucky childhood, to be the recipient<br />
+    Of a great effort to protect innocence, her<br />
+    Father’s aim to barricade her from incivility, and<br />
+    Foster an environment of creativity and love.<br />
+    Still, the world has a way of falling apart, if only<br />
+    To be reborn with new vigor.<br />
+    The entire village was stunned by the accusation,<br />
+    Assuredly false, that Maria’s father was guilty, then<br />
+    Sentenced to be hanged.<br />
+    The tears flowed as though Thales of Miletus<br />
+    Found the source of his watery archê in Maria.<br />
+    Her papa was gone.<br />
+    Her happy childhood abruptly ended. Yet,<br />
+    Maria’s newly ashen heart was determined<br />
+    To beat vibrant-red again someday.<br />
+    She would live to make her papa proud, for<br />
+    He taught her—<br />
+    A beautiful life is a choice.
+    `
+
+    questionTime = `
+    He walks slowly along the Socratic forest trail<br />
+    Happily enjoying several steps without pain,<br />
+    The ritual of ontological contemplation<br />
+    Coloring nature green, as the<br />
+    First autumn yellow leaves question time.<br />
+    Amid the placid freedom is awareness<br />
+    Of the river song shaping the rocks, but also<br />
+    Acknowledgement of the dark empire’s evil energy<br />
+    Dropping bombs on innocent children for profit.<br />
+    How unjust is this paradise offering moments of<br />
+    Windless warmth from our benevolent star, while<br />
+    Terrorizing untold peoples for differing politics?<br />
+    Maybe injustice cannot be banished, but<br />
+    Indifference to unnecessary suffering allows<br />
+    Those that control the money supply<br />
+    To continue their unconscionable experiments.<br />
+    Decentralize money for more fairness. 
+    `
+
+    flaubertianStruggle = `
+    Our time here is so tender, yet cruel, a type of<br />
+    Flaubertian struggle to be sure. For the<br />
+    Same riverbank water blessing the tree<br />
+    Undercuts its roots flowing round the bend,<br />
+    Precipitating an overearly death, as the tree<br />
+    Falls in slow motion<br />
+    Toward that which giveth and taketh away,<br />
+    Its life source, and saboteur.<br />
+    Knowing intensifies the grief.  
+    `
+
+    socialAmnesia = `
+    The desert knows it to be true<br />
+    That we’re much older<br />
+    Than our memories permit us to be, that<br />
+    Wind lifts birds free from the tree, and<br />
+    Rapids make storied kayak streams.<br />
+    What if our nature was peaceful, serene?<br />
+    Hiking over cactus<br />
+    Hiking, hiking over cactus, over cactus.<br />
+    The human story is perhaps incomplete,<br />
+    Maybe there existed more beautiful people<br />
+    Smiling and praying in families of antiquity<br />
+    Than science asks us to believe,<br />
+    Ravens scream murderous themes.<br />
+    Take every chance for love dear flower<br />
+    Stand tall and be brave in all weather, for a<br />
+    Taurid meteor might cause social amnesia,<br />
+    Tomorrow's unimaginable loss of our<br />
+    Triumphs, miseries, collective history,<br />
+    As those that happened before us<br />
+    Evanescent in the ether between stars.  
+    `
+
 
     poems1() {
         let data = 
         [
             {id: "national-empire", img: "national-empire.jpg", title: "National Empire", poem: this.nationalEmpire},
-            {id: "sans-anesthesia", img: "rogue-wave.jpg", title: "Sans Anesthesia", poem: this.sansAnesthesia}
+            {id: "sans-anesthesia", img: "rogue-wave.jpg", title: "Sans Anesthesia", poem: this.sansAnesthesia},
+            {id: "good-intentions", img: "sagebrush.jpg", title: "Missteps of Good Intentions", poem: this.missteps},
+            {id: "false-horizon", img: "snow.jpg", title: "False Horizon", poem: this.falseHorizon},
+            {id: "here-before", img: "hereBefore.jpg", title: "Here Before", poem: this.hereBefore},
+            {id: "to-care", img: "toCare.jpg", title: "To Care", poem: this.toCare},
+            {id: "chaos-of-ripples", img: "chaosOfRipples.jpg", title: "Chaos of Ripples", poem: this.chaosOfRipples},
+            {id: "crippled-in-spirit", img: "crippledInSpirit.jpg", title: "Crippled in Spirit", poem: this.crippledInSpirit},
+            {id: "primordial-routine", img: "primordialRoutine.jpg", title: "Primordial Routine", poem: this.primordialRoutine},
+            {id: "a-beautiful-life", img: "aBeautifulLife.jpg", title: "A Beautiful Life", poem: this.aBeautifulLife},
+            {id: "question-time", img: "questionTime.jpg", title: "Question Time", poem: this.questionTime},
+            {id: "flaubertian-struggle", img: "flaubertianStruggle.jpg", title: "Flaubertian Struggle", poem: this.flaubertianStruggle},
+            {id: "social-amnesia", img: "socialAmnesia.jpg", title: "Social Amnesia", poem: this.socialAmnesia}
         ]
         return data
     }
