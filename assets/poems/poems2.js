cfb9885ab779d9c79252063c5eb72d063beb12fd
Brett Wilcox <brettwilcox@truckstop.com>
2020-04-14 20:32:49 -0600

    Page2 poems and images.

--- assets/poems/poems2.js
+++ assets/poems/poems2.js
@@ -11,15 +11,418 @@ export default class Poems2 {
     A look of surprise can be seen in the dead man's eyes for a time,<br />
     But a heavy heart has no claim on the gentle beast.   
     `
-    
+
+    aStranger = `
+    A stranger walks unknown in foreign lands<br />
+    To everyone but himself.<br />
+    Passing by a storefront he sees his own reflection<br />
+    And nods at the man with acquaintance,<br />
+    A smile is returned in kind.  
+    `
+
+    windPaints = `
+    Wind paints the frozen Alaska sky in sharp lines<br />
+    Extending the blistering snow beyond the rugged peaks<br />
+    Like arms reaching skyward in morning stretch,<br />
+    And uncloaking the often invisible demarcation<br />
+    Of upslope wind and dangerous recirculation<br />
+    That forms the snow cornice in space.<br />
+    The bird knows the wind well,<br />
+    He sees the varying flow with or without snow<br />
+    And navigates the mountain ridges accordingly.   
+    `
+
+    birds = `
+    Birds make a thin distinction<br />
+    Between the organized folly<br />
+    Of people and ants marching furiously fast<br />
+    Towards an uncertain end<br />
+    With a seriousness that undermines<br />
+    Real joy along the way.  
+    `
+
+    boundariesAreDrawn = `
+    Boundaries are drawn on a map,<br />
+    Then redrawn a thousand times more before the end,<br />
+    In a struggle that is perfectly human,<br />
+    Trekking near to the mirage of security<br />
+    During times of new anthems and fashion,<br />
+    On a planet that doesn’t take sides with the victor.<br />
+    Last I checked we both breathe the air<br />
+    And wake from sweet dreams with fresh smiles on occasion,<br />
+    And worry about things we discuss in soft voices,<br />
+    And plead with the sky to spare our disaster.<br />
+    When the final time-lapse is viewed at high speed<br />
+    Please let there be no lines between people.   
+    `
+
+    chaseAfterTheMystery = `
+    A child asks of the old groundskeeper<br />
+    Watering graceful gardens<br />
+    In amber desert sun,<br />
+    “Do you like watering trees?<br />
+    There are so many,<br />
+    It must take forever.”<br />
+    “There’s more to it than that,”<br />
+    Says the strong man with a kind voice,<br />
+    “Watering trees<br />
+    Waters more than the trees,<br />
+    It gives life to memories.<br />
+    I think it’s very nice, and<br />
+    I’m okay with that too.<br />
+    … Run along now,” the man motions with a wave,<br />
+    “Chase after the mystery,<br />
+    Look farther ahead than behind, and<br />
+    Watch out for rocks along the way.”  
+    `
+
+    fromTheBow = `
+    From the bow of a great and mighty ship,<br />
+    In a blackness mirrored by the moonless water,<br />
+    A profound and humbling incomprehension<br />
+    With the vastness of space<br />
+    Overwhelmed and mystified me;<br />
+    I became unbalanced in complete and terrifying wonderment<br />
+    Of the countless twinkling little lights<br />
+    Mapping their ancient riddled exhalations<br />
+    Across the universe to the sea.  
+    `
+
+    iFeelDifferently = `
+    I feel differently now<br />
+    Looking back on my life,<br />
+    About the risks I ventured in youth—<br />
+    When time was more likely,<br />
+    And strength more complete,<br />
+    When patience was weaker,<br />
+    And love everlasting, so I thought.<br />
+    Now the movie replaying in my tired mind<br />
+    Is mostly familiar smiling faces<br />
+    Laughing,<br />
+    Uncontrollable laughing,<br />
+    And the rain—<br />
+    The wretched damn rain<br />
+    The cold day that you died,<br />
+    And the new wildflowers<br />
+    That bloomed the next spring.   
+    `
+
+    letTheWater = `
+    Let the water move you<br />
+    Not the other way around,<br />
+    So that your efforts are rewarded<br />
+    In a living connection with nature,<br />
+    Which is near to grace and beauty.<br />
+    Do not force your will upon the river,<br />
+    Which is a temporary victory,<br />
+    Rather, rejoice in that line of water<br />
+    That tracks safely through the boulders<br />
+    And beyond the horizon line.   
+    `
+
+    andWhat = `
+    And what about your fixation with the past?<br />
+    Does it ever subside?<br />
+    Asked the man of himself.<br />
+    Not really, not often, not truly, answered he.<br />
+    And the record skipped back with a squeal,<br />
+    To the worn groove it sings in the song.  
+    `
+
+    hopsVines = `
+    Hops vines climb the patio posts<br />
+    Reaching the brown lattice stained top<br />
+    Creating a network of shadows below<br />
+    That codes new laughter and passion,<br />
+    As years pass by in a desert sunset. 
+    `
+
+    aManWalkingAlone = `
+    A tired man walking alone in the desert<br />
+    Happened upon a bird not startled by his approach<br />
+    So the curious man asked the bird how to fly,<br />
+    The old desert bird politely described what he could‒<br />
+    How he flapped his fledging wings for hours upon hours<br />
+    Afraid he wouldn’t survive the first height.<br />
+    Time passed and his patience diminished<br />
+    From long restless days in the nest,<br />
+    Until finally one day when he thought he was ready<br />
+    Deciding to give flight a try.<br />
+    The young bird steadied his nerve, and<br />
+    Hopped near to the edge<br />
+    Then lost suddenly his footing and fell,<br />
+    Crashing quickly toward rocks and soil<br />
+    He spread his wings hard,<br />
+    Bracing for certain demise.<br />
+    How frightening, said the man, interrupting the story<br />
+    Impressed by the birds explanation,<br />
+    Thinking himself close to the knowledge of flight.<br />
+    Yes, very scary indeed, said the bird,<br />
+    A very close call with disaster,<br />
+    But that has nothing to do with how to fly,<br />
+    And gets you no nearer the answer.<br />
+    It probably won’t make much sense to you, said the wise bird<br />
+    But after all these years in the sky<br />
+    I know it’s less about wings than the wind.   
+    `
+
+    aThousand = `
+    A thousand men fight<br />
+    For the love of a queen<br />
+    And kill for the love of control,<br />
+    Plotting red death to a coward king on the run<br />
+    Battered armies courageous in struggle,<br />
+    Bishop takes pawn on f4.<br />
+    ...Check.  
+    `
+
+    earth = `
+    Earth has wild wants too,<br />
+    Evolving art-life forms unending<br />
+    Replacing desert canvas for new shades of green<br />
+    Eroding rocky coastline with sandy beach dreams<br />
+    Loving the forest in fresh flowing streams<br />
+    Breathing sharp snow-topped arete mountain scenes<br />
+    Carving canyon deep feelings in minds spinning free<br />
+    Caressing her lover in orbiting rings<br />
+    While rolling together in darkness.  
+    `
+
+    iRefuse = `
+    I refuse to find a balance<br />
+    In my love for you,<br />
+    I want you all the way.  
+    `
+
+    heRan = `
+    He ran with cold wind<br />
+    Burning his skin,<br />
+    Towards his sweet wife<br />
+    Hurt by cruel words,<br />
+    As the train departed their love.<br />
+    In life sometimes you are late.   
+    `
+
+    theGreatDesire = `
+    The great desire we all share<br />
+    Is our want to love and be loved,<br />
+    That’s why death stabs our hearts so sharply.<br />
+    You don’t get over death<br />
+    You just keep moving downstream—<br />
+    Water in a river on a journey to the sea.  
+    `
+
+    thunderStorms = `
+    Thunder storms darken the daylight<br />
+    Bottles of vineyards pile deep<br />
+    As lover’s emotions unstable<br />
+    Promising curtains of rain and mischief.  
+    `
+
+    youWillKnowMe = `
+    Written words<br />
+    Will<br />
+    Live on,<br />
+    Beyond<br />
+    My final heartbeat.<br />
+    And from that horrible distance<br />
+    You will know me<br />
+    To be true.   
+    `
+
+    canyonAlpenglow = `
+    Orange fades to canyon alpenglow<br />
+    Hearkening hope in dim distant stars,<br />
+    Emboldening embers impassioned in arms,<br />
+    Flickering cracks, and bursts from heat throes,<br />
+    Shifting winds and my thoughts down the slope<br />
+    Of her back as we take off our clothes<br />
+    Beneath naked skies only lovers have known.  
+    `
+
+    unexpectedDeparture = `
+    Autumn rain wetted her hair<br />
+    Soft, like a morning kiss,<br />
+    Adding chill to the tragedy, and<br />
+    Blistering every solemn footstep<br />
+    As she walked alone in great grief<br />
+    From an unexpected departure with love.  
+    `
+
+    desertDetermination = `
+    A tired old tree<br />
+    Born many years ago on the plateau,<br />
+    Brittle with suffering from the warming desert, and<br />
+    Unaware of a mass looming lasting displacement,<br />
+    Holds firm to poor, thirsty earth<br />
+    Hoping others are buttressed<br />
+    By her desert determination to be, and<br />
+    Uncertain her fate should she fail.   
+    `
+
+    bornInDryDesert = `
+    Clouds mushroom sprout the horizon<br />
+    Finding freedom in less pressure on high,<br />
+    Splash blue with white elephant billows of love,<br />
+    Turn dark as new monsters, and<br />
+    Rage wild through the night.<br />
+    Morphing sky white afternoon treasures<br />
+    Born in dry desert air light,<br />
+    Rise angry in evening late concert<br />
+    Clapping bright thunder with lightning loud fright.  
+    `
+
+    firstRaindrops = `
+    Maria Flores was born with an uncommon share of beauty,<br />
+    Uncommon even for the beautiful women of Monterrey, Mexico.<br />
+    She was born the same day the drought ended,<br />
+    Her newborn tears spilling onto the hospital floor<br />
+    As the first raindrops blessed the parched flora of her homeland;<br />
+    Her wails the harbinger of the echoing thunder and lightning<br />
+    That brightened the stormy city skyline.<br />
+    When she was young Maria’s father would recount the night of her birth.<br />
+    He said the fires that raged throughout the countryside that year<br />
+    Were swallowed up by her spirit,<br />
+    That she saved the city, and<br />
+    Brought joy not only to her family but to an entire generation.<br />
+    The story always made Maria feel special.<br />
+    And such was her father’s talent,<br />
+    To make her feel as though she were a gift to the world.  
+    `
+
+    rightNow = `
+    It's a lie that time heals wounds,<br />
+    It doesn't.<br />
+    In fact, time itself is a great lie and doesn't exist.<br />
+    Time is an ironical part of our subjective, conscious reality,<br />
+    It doesn't exist outside our minds.<br />
+    Things only ever happen right now<br />
+    And the present moment is all you ever get.<br />
+    The past is a memory and the future only a hope or fear,<br />
+    But neither exists right now.  
+    `
+
+    objectiveView = `
+    Birds feel for the wind<br />
+    Bent willingly towards thermal skies<br />
+    Ascending nearer an objective view<br />
+    Of a planet wreaked in mayhem,<br />
+    Spying a species killing and cruel,<br />
+    Fighting for their color,<br />
+    Fighting for their food,<br />
+    Fighting over Gods<br />
+    Created by their fears.<br />
+    Soaring high above the desert<br />
+    Birds know not to care, they find<br />
+    Upwind freedom from the chaos<br />
+    Crosswind freedom from the grind<br />
+    Downwind freedom from the worries<br />
+    Absorbed in peoples’ minds.<br />
+    Birds say give me flight<br />
+    Man says give me more,<br />
+    Lift is grace upon their feathers<br />
+    Wailing ailing tearstained faces,<br />
+    Diving desert brown escarpments<br />
+    Daily dying beneath bombardments,<br />
+    Is a life for each to choose.<br />
+    Birds feel for the wind.  
+    `
+
+    livePoetically = `
+    Poetry is life in words.<br />
+    Surely your life is poetry<br />
+    If only you learned to love the words.<br />
+    Words are meaning<br />
+    Words are experiences<br />
+    Beautiful, eternal, ethereal words make meanings memorable<br />
+    And worth our might and struggle.<br />
+    Write about your life, because<br />
+    If you’re not telling a story you’re not living right.<br />
+    Live poetically.  
+    `
+
+    yakutatBayCrossing = `
+    The music grew louder, softer, the perfect pitch<br />
+    From the fog gray front between heartbeat and not.<br />
+    Committed? Yes!<br />
+    What a slippery slope decisions and such,<br />
+    And gravity the arbiter.<br />
+    I am the test<br />
+    Bravado no better than calm and sense of purpose.<br />
+    Focus<br />
+    Feel the waves alive within<br />
+    Dare to trust.<br />
+    The music serene, sublime, if even music there was.<br />
+    Aware of all possible outcomes—<br />
+    Frigid water, cold knowledge of people left behind<br />
+    Eternally important.<br />
+    But the music is life and together we dance,<br />
+    The harmony so smooth, so clear<br />
+    And nothing else.<br />
+    Then, after a lifetime and no time<br />
+    And then, after enduring all that could have been<br />
+    Oh sweet land—<br />
+    My smile<br />
+    My reward,<br />
+    Where all my love resides.  
+    `
+
+    painfullyTrue = `
+    A regrettable, honest truth about life:<br />
+    Leaving loved ones gets more difficult over time<br />
+    And not easier with practice, as one would expect,<br />
+    And I found it to be painfully true as I've aged.  
+    `
+
+    laSilla = `
+    Cerro de la Silla<br />
+    The Saddle,<br />
+    Nest of the morning Mexico sun,<br />
+    Stalwart reminder of a proud tradition,<br />
+    Homage to a struggling culture,<br />
+    Signpost of the Sierra Madre Oriental range.<br />
+    A mountain of hope.  
+    `
+
+
 
 
     poems2() {
         let data = 
         [
             {id: "a-menacing-knight", img: "aMenacingKnight.jpg", title: "A Menacing Knight", poem: this.aMenacingKnight},
+            {id: "a-stranger", img: "aStranger.jpg", title: "A Stranger", poem: this.aStranger},
+            {id: "wind-paints", img: "windPaints.jpg", title: "Wind Paints", poem: this.windPaints},
+            {id: "birds", img: "birds.jpg", title: "Birds", poem: this.birds},
+            {id: "boundaries-are-drawn", img: "boundariesAreDrawn.jpg", title: "Boundaries are Drawn", poem: this.boundariesAreDrawn},
+            {id: "chase-after-the-mystery", img: "chaseAfterTheMystery.jpg", title: "Chase After the Mystery", poem: this.chaseAfterTheMystery},
+            {id: "from-the-bow", img: "fromTheBow.jpg", title: "From the Bow", poem: this.fromTheBow},
+            {id: "i-feel-differently", img: "iFeelDifferently.jpg", title: "I Feel Differently", poem: this.iFeelDifferently},
+            {id: "let-the-water", img: "letTheWater.jpg", title: "Let the Water", poem: this.letTheWater},
+            {id: "and-what", img: "andWhat.jpg", title: "And What", poem: this.andWhat},
+            {id: "hops-vines", img: "hopsVines.jpg", title: "Hops Vines", poem: this.hopsVines},
+            {id: "a-man-walking-alone", img: "aManWalkingAlone.jpg", title: "A Man Walking Alone", poem: this.aManWalkingAlone},
+            {id: "a-thousand", img: "aThousand.jpg", title: "A Thousand", poem: this.aThousand},
+            {id: "earth", img: "earth.jpg", title: "Earth", poem: this.earth},
+            {id: "i-refuse", img: "iRefuse.jpg", title: "I Refuse", poem: this.iRefuse},
+            {id: "he-ran", img: "heRan.jpg", title: "He Ran", poem: this.heRan},
+            {id: "the-great-desire", img: "theGreatDesire.jpg", title: "The Great Desire", poem: this.theGreatDesire},
+            {id: "thunder-storms", img: "thunderStorms.jpg", title: "Thunder Storms", poem: this.thunderStorms},
+            {id: "you-will-know-me", img: "youWillKnowMe.jpg", title: "You Will Know Me", poem: this.youWillKnowMe},
+            {id: "canyon-alpenglow", img: "canyonAlpenglow.jpg", title: "Canyon Alpenglow", poem: this.canyonAlpenglow},
+            {id: "unexpected-departure", img: "unexpectedDeparture.jpg", title: "Unexpected Departure", poem: this.unexpectedDeparture},
+            {id: "desert-determination", img: "desertDetermination.jpg", title: "Desert Determination", poem: this.desertDetermination},
+            {id: "born-in-dry-desert", img: "bornInDryDesert.jpg", title: "Born in Dry Desert", poem: this.bornInDryDesert},
+            {id: "first-raindrops", img: "firstRaindrops.jpg", title: "First Raindrops", poem: this.firstRaindrops},
+            {id: "right-now", img: "rightNow.jpg", title: "Right Now", poem: this.rightNow},
+            {id: "objective-view", img: "objectiveView.jpg", title: "Objective View", poem: this.objectiveView},
+            {id: "live-poetically", img: "livePoetically.jpg", title: "Live Poetically", poem: this.livePoetically},
+            {id: "yakutat-bay-crossing", img: "yakutatBayCrossing.jpg", title: "Yakutat Bay Crossing", poem: this.yakutatBayCrossing},
+            {id: "painfully-true", img: "painfullyTrue.jpg", title: "Painfully True", poem: this.painfullyTrue},
+            {id: "la-silla", img: "laSilla.jpg", title: "La Silla", poem: this.laSilla},
+            {id: "", img: "birds.jpg", title: "Birds", poem: this.birds},
 
-            
+
 
 
         ]
